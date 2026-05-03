6add2f1ab132ea9be47221a5f51c297948c5d9c8
Andrés Contreras Guillén <ancongui@Andress-MacBook-Pro.local>
2026-05-03 12:55:02 +0200

    chore: rebrand copyright to Firefly Software Foundation
    Update copyright notices, attributions, and references from
    Firefly Software Solutions Inc to Firefly Software Foundation
    in preparation for the Apache 2.0 release under the
    Firefly Software Foundation.

--- src/main/java/com/vaadin/starter/business/backend/dto/loanoperations/LoanCollectionDTO.java
+++ src/main/java/com/vaadin/starter/business/backend/dto/loanoperations/LoanCollectionDTO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Firefly Software Solutions Inc
+ * Copyright 2025 Firefly Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
